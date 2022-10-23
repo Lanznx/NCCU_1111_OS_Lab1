a5ebc96ec1d025833df3eaa188e7f26c28cc2f9c
Lanznx <109305092@g.nccu.edu.tw>
2022-10-23 04:30:58 +0000

    finish assignment

--- src/seconds.c
+++ src/seconds.c
@@ -39,13 +39,9 @@ static struct file_operations proc_ops = {
 // 當模組被載入會觸發這個函式 返回0代表成功 其他值代表失敗
 static int proc_init(void)
 {
-    
-    //////////////////////
-    ////<在此加入程式碼>////
-    /////////////////////
-    //用變數記住載入模組時的jiffies
-    
-    
+	init_jiffies = jiffies;
+	printk(KERN_INFO "init_jiffies: %lu\n", jiffies);
+
         // 創出/proc/seconds這個項目
         proc_create(PROC_NAME, 0, NULL, &proc_ops);
 
@@ -82,11 +78,8 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
         // 計算目前jiffies和模組初始jiffies的差，並除以HZ值
         // 將結果透過sprintf放到buffer中，使用%ld印出
         rv = sprintf(buffer,
-                     "Module has been running for %ld seconds\n",
-                     //////////////////////
-                     ////<在此加入程式碼>////
-                     /////////////////////
-                     );
+                     "Module has been running for %ld seconds\n", (jiffies - init_jiffies) / HZ
+		     );
 
     
         // 將buffer的內容複製到用戶空間usr_buf
